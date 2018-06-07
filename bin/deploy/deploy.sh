43c2709250ddeef0b3e60d34ce65b3aac61833d8
VesnaVK <vesna@vesnakovach.com>
2018-06-06 23:42:09 -0500

    no db

--- bin/deploy/deploy.sh
+++ bin/deploy/deploy.sh
@@ -1,7 +1,8 @@
 sudo rm /var/www/crm && \
 sudo ln -s /var/www/crm_current /var/www/crm && \
 cd /var/www/crm && \
-sudo APP_ENV=$APP_ENV DATABASE_URL=$DATABASE_URL php bin/console doctrine:migrations:migrate --no-interaction && \
+# sudo APP_ENV=$APP_ENV DATABASE_URL=$DATABASE_URL php bin/console doctrine:migrations:migrate --no-interaction && \
+sudo APP_ENV=$APP_ENV DATABASE_URL=$DATABASE_URL && \
 sudo chown -R www-data:www-data /var/www/crm_current && \
 sudo chown -h www-data:www-data /var/www/crm && \
 sudo service apache2 restart
