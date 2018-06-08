69a03cdaa024aa05ed2b19aa2b5f72a988e83d03
VesnaVK <vesna@vesnakovach.com>
2018-06-07 20:32:53 -0500

    Handled potentially missing folder during deploy

--- bin/deploy/pre_deploy.sh
+++ bin/deploy/pre_deploy.sh
@@ -1,5 +1,5 @@
 # Remove symlink
-sudo rm -R /var/www/crm_old && \
+sudo rm -Rf /var/www/crm_old && \
 sudo cp -R /var/www/crm_current /var/www/crm_old/ && \
 sudo rm /var/www/crm && \
 sudo rm -R /var/www/crm_current && \
